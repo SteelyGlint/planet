78eda1fcb85caa7b7d3438a8ef0ca8aef056134e
SteelyGlint <bwiegner@gmail.com>
2017-08-12 22:30:35 -0400

    Moving towards usefulness
    More encapsulated class design
    Surface generation in one step, rather than before your eyes
    Ecology displayed as a biome instead of an overlay

--- planetgen.gd
+++ planetgen.gd
@@ -2,31 +2,22 @@ extends TextureFrame
 
 var surfaceImage
 var surfaceTexture
-var ecologyImage
-var ecologyTexture
 var imageSize = Vector2(100, 100)
 var imageFormat = Image.FORMAT_RGBA
 
-var planetsize = 21
+var planetsize = 27
 var planetwidth = int(planetsize*1.6) 
 var chamfer = floor(planetsize * 0.4)
 var totalsquares = 0
+var landspawns = []
+var hillspawns = []
 var landsquares = 0
 var hillsquares = 0
-var landfraction = .5
+var landfraction = .35
 var hillfraction = landfraction * 0.5
 var landspreadfactor = 0.5
-var landstarts = int(planetsize*0.45)
+var landstarts = ceil(planetsize*.45)
 var display_scale = 8
-var color_ocean = Color(.15, .45, .70) # - dry ocean Color(.88, .68, .28) 
-#var color_land = Color(.21, .64, .23)
-#var color_hill = Color(.26, .54, .23)
-#var color_mount = Color(.29, .46, .21)
-var color_land = Color(.80, .60, .20)
-var color_hill = Color(.70, .51, .21)
-var color_mount = Color(.55, .31, .18)
-var color_ice = Color(.89, .96, .99)
-var color_red = Color(.9, .3, .3)
 
 var tiles = []
 var Tile = preload("tile.gd")
@@ -48,12 +39,6 @@ func _ready():
 	surfaceTexture.set_data(surfaceImage)
 	set_texture(surfaceTexture)
 	
-	ecologyImage = Image(imageSize.x, imageSize.y, false, imageFormat)
-	ecologyTexture = ImageTexture.new()
-	ecologyTexture.create(imageSize.x,imageSize.y, imageFormat, 0)    
-	ecologyTexture.set_data(surfaceImage)
-	get_node("ecology").set_texture(ecologyTexture)
-	
 	set_scale(Vector2(display_scale, display_scale))
 	set_pos(Vector2(25, 25))
 	
@@ -65,16 +50,7 @@ func _ready():
 		for yy in range(planetsize):
 			tiles[xx].append(Tile.new())
 	
-	for xx in range(planetwidth):
-		for yy in range(planetsize):
-			if pix_on_surface(xx, yy):
-				surfaceImage.put_pixel(xx,yy, color_ocean)
-				surfaceTexture.set_data(surfaceImage)
-				totalsquares += 1
-				tiles[xx][yy].biome = Tile.ocean
-
-var landspawns = []
-var hillspawns = []
+	create_surface()
 
 func get_adjacent(xx, yy):
 	var adj_list = []
@@ -112,68 +88,94 @@ func step_east(xx, yy):
 
 func try_insert_land(xx, yy):
 	if pix_on_surface(xx, yy) and randf()<landspreadfactor and tiles[xx][yy].biome != Tile.land:
-		surfaceImage.put_pixel(xx,yy, color_land)
 		landsquares += 1
 		tiles[xx][yy].biome = Tile.land
 		landspawns.push_back( Vector2(xx, yy) )
 
 func try_insert_hill(xx, yy):
 	if pix_on_surface(xx, yy) and randf()<0.8 and tiles[xx][yy].biome != Tile.hill:
-		surfaceImage.put_pixel(xx,yy, color_hill)
 		hillsquares += 1
 		tiles[xx][yy].biome = Tile.hill
 		
 		var adj_list = get_adjacent( xx, yy )
 		for adj in adj_list:
-			if tiles[adj.x][adj.y].biome != Tile.land and randf()<0.4: return
+			if tiles[adj.x][adj.y].biome == Tile.ocean and randf()<0.5: return
 		
 		hillspawns.push_back( Vector2(xx, yy) )
 
-var mountainflag = false
-func drop_land_square():
-	if landsquares < landstarts:
+func create_surface():
+	# lay down the ocean
+	for xx in range(planetwidth):
+		for yy in range(planetsize):
+			if pix_on_surface(xx, yy):
+				totalsquares += 1
+				tiles[xx][yy].biome = Tile.ocean
+	
+	# create points from which continents will grow
+	while landsquares < landstarts:
 		var xx = randi()%(planetwidth)
-		var yy = randi()%planetsize
+		var yy = 2 + randi()%(planetsize-4)
 		if pix_on_surface(xx, yy):
-			surfaceImage.put_pixel(xx,yy, color_land)
 			landspawns.push_back( Vector2(xx, yy) )
 			hillspawns.push_back( Vector2(xx, yy) )
 			landsquares += 1
 			tiles[xx][yy].biome = Tile.land
-	elif landsquares<landfraction*totalsquares and landspawns.size() > 0:
+	
+	# grow initial landmasses
+	while landsquares<landfraction*totalsquares and landspawns.size() > 0:
 		var spread_land = landspawns[0]
 		landspawns.pop_front()
 		var adj_list = get_adjacent( spread_land.x, spread_land.y )
 		for adj in adj_list:
 			try_insert_land( adj.x, adj.y )
-	elif hillsquares<hillfraction*totalsquares and hillspawns.size() > 0:
+	
+	# grow hills within the landmasses
+	while hillsquares<hillfraction*totalsquares and hillspawns.size() > 0:
 		var spread_land = hillspawns[0]
 		hillspawns.pop_front()
 		var adj_list = get_adjacent( spread_land.x, spread_land.y )
 		for adj in adj_list:
 			try_insert_hill( adj.x, adj.y )
-	elif not mountainflag:
-		for xx in range(planetwidth):
-			for yy in range(1, planetsize-1):
-				if pix_on_surface(xx, yy):
-					var mountain_chance = 0
-					var adj_list = get_adjacent( xx, yy )
-					for adj in adj_list:
-						if tiles[adj.x][adj.y].biome == Tile.hill: mountain_chance += 10
-						if tiles[adj.x][adj.y].biome == Tile.mount: mountain_chance += 30
-						if tiles[adj.x][adj.y].biome == Tile.land: mountain_chance += -25
-						if tiles[adj.x][adj.y].biome == Tile.ocean: mountain_chance += -100
-					if randi()%100 < mountain_chance:
-						surfaceImage.put_pixel(xx,yy, color_mount)
-						tiles[xx][yy].biome = Tile.mount
-		surfaceTexture.set_data(surfaceImage)
-		mountainflag = true
+
+	# set a few squares deep in the hills to mountains
+	for xx in range(planetwidth):
+		for yy in range(1, planetsize-1):
+			if pix_on_surface(xx, yy):
+				var mountain_chance = 0
+				var adj_list = get_adjacent( xx, yy )
+				for adj in adj_list:
+					if tiles[adj.x][adj.y].biome == Tile.hill: mountain_chance += 8
+					if tiles[adj.x][adj.y].biome == Tile.mount: mountain_chance += 30
+					if tiles[adj.x][adj.y].biome == Tile.land: mountain_chance += -30
+					if tiles[adj.x][adj.y].biome == Tile.ocean: mountain_chance += -100
+					if tiles[adj.x][adj.y].biome == Tile.drybed: mountain_chance += -100
+				if randi()%100 < mountain_chance:
+					tiles[xx][yy].biome = Tile.mount
+	
+	# apply ice caps to the poles
+	for xx in range(planetwidth):
+		for yy in [0, 1, 2, 3, planetsize-4, planetsize-3, planetsize-2, planetsize-1]:
+			if pix_on_surface(xx, yy):
+				var icyness = 0
+				if yy == 0 or yy == 1 or yy == planetsize-2 or yy == planetsize-1: icyness += 3
+				if yy == 2 or yy == planetsize-3: icyness += 2
+				if yy == 3 or yy == planetsize-4: icyness += 1
+				if tiles[xx][yy].biome == Tile.mount: icyness += 2
+				if tiles[xx][yy].biome == Tile.hill: icyness += 1
+				if tiles[xx][yy].biome == Tile.ocean and randf()<0.5: icyness += -1
+				if icyness >= 2:
+					if tiles[xx][yy].biome == Tile.ocean:
+						tiles[xx][yy].biome = Tile.seaice
+					else:
+						tiles[xx][yy].biome = Tile.ice
+	
+	add_moisture()
+	create_texture()
 
 func get_tile_dank(type):
 	if type == Tile.ocean:	return 2
 	else:					return 0
 
-var moistflag = false
 func add_moisture():
 	var moist = []
 	for xx in range(planetwidth):
@@ -190,11 +192,8 @@ func add_moisture():
 					dank += get_tile_dank( tiles[adj.x][adj.y].biome )
 				dank = dank / (2.0 + adj_list.size())
 				moist[xx][yy] = dank
-				
-				#ecologyImage.put_pixel(xx,yy, Color(0.2, 0.5, 0.3, dank*0.5))
-				#ecologyTexture.set_data(ecologyImage)
 	
-	for iter in range(6):
+	for iter in range(5):
 		for xx in range(planetwidth):
 			for yy in range(planetsize):
 				if pix_on_surface(xx, yy):
@@ -222,22 +221,24 @@ func add_moisture():
 					
 					if (tiles[xx][yy].moisture > 0.1 and tiles[xx][yy].biome == Tile.land) or\
 							(tiles[xx][yy].moisture > 0.5 and tiles[xx][yy].biome != Tile.mount): 
-						#ecologyImage.put_pixel(xx,yy, Color(0.82, 0.25, 0.93, 0.6))
-						#ecologyImage.put_pixel(xx,yy, Color(0.62, 0.15, 0.13, 0.6))
-						#ecologyImage.put_pixel(xx,yy, Color(0.3, 0.1, 0.5, 0.6))
-						ecologyImage.put_pixel(xx,yy, Color(0.25, 0.45, 0.25, 0.6))
-					ecologyTexture.set_data(ecologyImage)
+						tiles[xx][yy].biome = Tile.terran
 
+func create_texture():
+	for xx in range(planetwidth):
+		for yy in range(planetsize):
+			if pix_on_surface(xx, yy):
+				surfaceImage.put_pixel(xx,yy, tiles[xx][yy].color())
+
+var temp = true
 func _process(d):
 	total_d += d
 	
-	if total_d > 0.5:
-		for ii in range(10):
-			drop_land_square()
+	#if total_d > 0.5:
+	#	for ii in range(50):
+	#		drop_land_square()
 	
-	if total_d > 2.5 and not moistflag:
-		add_moisture()
-		moistflag = true
+	#if total_d > 2.5:
+	#	create_texture()
 	
 	surfaceTexture.set_data(surfaceImage)
 
--- tile.gd
+++ tile.gd
@@ -4,19 +4,31 @@ var moisture = 0
 
 const none = 0
 const ocean = 1
-const land = 2
-const hill = 3
-const mount = 4
-const ice = 5
+const drybed = 2
+const land = 3
+const hill = 4
+const mount = 5
+const ice = 6
+const seaice = 7
+const terran = 8
+const xeno = 9
+
+# green versions
+#var color_land = Color(.21, .64, .23)
+#var color_hill = Color(.26, .54, .23)
+#var color_mount = Color(.29, .46, .21)
 
 func color():
-	if biome == none: return Color( 0, 0, 0, 0 )
-	if biome == ocean: return Color( 0, 0, 0, 0 )
-	if biome == land: return Color( 0, 0, 0, 0 )
-	if biome == hill: return Color( 0, 0, 0, 0 )
-	if biome == mount: return Color( 0, 0, 0, 0 )
-	if biome == ice: return Color( 0, 0, 0, 0 )
-	if biome == none: return Color( 0, 0, 0, 0 )
+	if biome == none: return Color(0, 0, 0, 0)
+	if biome == ocean: return Color(.15, .45, .70)
+	if biome == drybed: return Color(.88, .68, .28)
+	if biome == land: return Color(.80, .60, .20)
+	if biome == hill: return Color(.70, .51, .21)
+	if biome == mount: return Color(.55, .31, .18)
+	if biome == ice: return Color(.89, .96, .99)
+	if biome == seaice: return Color(.68, .89, .99)
+	if biome == terran: return  Color(0.4, 0.5, 0.15)
+	if biome == xeno: return Color(.38, .18, .63)
 
 func _init():
 	pass
